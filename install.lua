61da71f970c6bed40e85358d2d4a7d11f6c3473b
Codermsk <62512817+Codermsk@users.noreply.github.com>
2022-11-01 02:27:04 +0300

    Update install.lua

--- install.lua
+++ install.lua
@@ -3,6 +3,7 @@ local REP = "https://raw.githubusercontent.com/Codermsk/CasMcSkill/master"
 local shell = require("shell")
 shell.execute("wget -fq " .. REP .. "/launcher.lua /home/1.lua")
 shell.execute("wget -fq " .. REP .. "/libs/casino.lua /lib/casino.lua")
-shell.execute("wget -fq " .. REP .. "/config/settings.lua /lib/settings.lua")
 shell.execute("wget -fq " .. REP .. "/config/games.lua /lib/games.lua")
+shell.execute("wget -fq " .. REP .. "/config/settings.lua /lib/settings.lua")
+
 shell.execute("edit /lib/settings.lua")
