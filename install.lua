fe2f446d93dec2d5e14cbb9a23c1224a2ef28773
Codermsk <62512817+Codermsk@users.noreply.github.com>
2022-11-01 01:48:33 +0300

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,7 +1,7 @@
-local REPOSITOTY = "https://raw.githubusercontent.com/Codermsk/CasMcSK/master"
+local REP = "https://raw.githubusercontent.com/Codermsk/CasMcSK/master"
 
 local shell = require("shell")
-shell.execute("wget -fq " .. REPOSITOTY .. "/launcher.lua /home/1.lua")
-shell.execute("wget -fq " .. REPOSITOTY .. "/libs/casino.lua /lib/casino.lua")
-shell.execute("wget -fq " .. REPOSITOTY .. "/config/settings.lua /lib/settings.lua")
+shell.execute("wget -fq " .. REP .. "/launcher.lua /home/1.lua")
+shell.execute("wget -fq " .. REP .. "/libs/casino.lua /lib/casino.lua")
+shell.execute("wget -fq " .. REP .. "/config/settings.lua /lib/settings.lua")
 shell.execute("edit /lib/settings.lua")
