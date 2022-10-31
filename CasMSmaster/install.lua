f2b9fcbfba2c0089966b824fc644d69fc98db631
Codermsk <62512817+Codermsk@users.noreply.github.com>
2022-11-01 01:35:42 +0300

    Delete CasMSmaster directory

--- CasMSmaster/install.lua
+++
@@ -1,7 +0,0 @@
-local REPOSITOTY = "https://raw.githubusercontent.com/Codermsk/CasMcSK/master"
-
-local shell = require("shell")
-shell.execute("wget -fq " .. REPOSITOTY .. "/launcher.lua /home/1.lua")
-shell.execute("wget -fq " .. REPOSITOTY .. "/libs/casino.lua /lib/casino.lua")
-shell.execute("wget -fq " .. REPOSITOTY .. "/config/settings.lua /lib/settings.lua")
-shell.execute("edit /lib/settings.lua")
